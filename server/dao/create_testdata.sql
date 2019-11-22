d8f929097ec3992732a46faefffc786973423e20
magnubau <magnubau@stud.ntnu.no>
2019-11-22 19:27:27 +0100

    Laga articledao-tester

--- server/dao/create_testdata.sql
+++ server/dao/create_testdata.sql
@@ -6,8 +6,8 @@ values ('Testartikkel', 'Dette er en viktig artikkel \n veldig!', 'https://media
         'Arthur Morgan', 1, 2, 'A beautifull man');
 
 insert into artikkel (tittel, tekst, bilde, forfatter, viktighet, kategoriid, alt)
-values ('Testartikkel', 'Dette er en viktig artikkel \n veldig!', 'https://media.comicbook.com/2018/10/red-dead-redemption-2-1141343.jpeg?auto=webp&width=696&height=390&crop=696:390,smart',
+values ('Testartikkel2', 'Dette er en viktig artikkel \n veldig!', 'https://media.comicbook.com/2018/10/red-dead-redemption-2-1141343.jpeg?auto=webp&width=696&height=390&crop=696:390,smart',
         'Arthur Morgan', 1, 1, 'A beautifull man');
 
 insert into kommentar (nickname, tekst, artikkelid) VALUES ('Curious Betsy', 'Fin artikkel', 1);
-insert into kommentar (nickname, tekst, artikkelid) VALUES ('Mag', 'dårlig artikkel', 2);
+insert into kommentar (nickname, tekst, artikkelid) VALUES ('Curious Bob', 'Fin artikkel', 1);
